3cb424565d5b2358a28ea7c2766ede1c3b2b49fd
v-supermodule <105209876+v-supermodule@users.noreply.github.com>
2022-10-08 01:37:39 +0400

    Fixed basePath and assetPrefix path

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath : isProd ? '/app' : '',
-  assetPrefix : isProd ? '/app' : ''
+  basePath : isProd ? '/' : '',
+  assetPrefix : isProd ? '/' : ''
 }
 
 module.exports = nextConfig
