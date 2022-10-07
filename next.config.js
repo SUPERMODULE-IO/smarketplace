09c3696c9d0305e88d7a568acd9429910b90bace
v-supermodule <105209876+v-supermodule@users.noreply.github.com>
2022-10-08 01:53:07 +0400

    Fixed fonts path

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath : isProd ? '/' : '',
-  assetPrefix : isProd ? '/' : ''
+  basePath : isProd ? '' : '',
+  assetPrefix : isProd ? '' : ''
 }
 
 module.exports = nextConfig
